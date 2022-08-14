88c0e990695d54e1ce014148de0644693ea35862
Matheus Montenegro <matheusalbuquerquemontenegro@hotmail.com>
2022-08-13 23:34:02 -0300

    ajuste campo estado endereco

--- src/main/java/ifpe/br/model/Endereco.java
+++ src/main/java/ifpe/br/model/Endereco.java
@@ -1,17 +1,7 @@
 package ifpe.br.model;
 
 import javax.persistence.Embeddable;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 
-import ifpe.br.enums.Estados;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 @Embeddable
 public class Endereco {
 	private String cep;
@@ -19,7 +9,54 @@ public class Endereco {
 	private String numero;
 	private String bairro;
 	private String cidade;
-	@Enumerated(EnumType.STRING)
-	private Estados estado;
+	private String estado;
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
 
 }
